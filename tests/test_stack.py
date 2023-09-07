60c59bf0311caf65441b27156d7b72f55d3ecd18
ViktorDavydov <davydov.viktor.andreevich@gmail.com>
2023-09-07 22:06:25 +0300

    first commit

--- tests/test_stack.py
+++ tests/test_stack.py
@@ -1,16 +1,20 @@
+# imports
 import unittest
 from src.stack import Node, Stack
 
 
 class TestStack(unittest.TestCase):
+    """"Test class initializing"""
 
     def test_Node(self):
+        """Class Node testing"""
         n1 = Node(5, None)
         n2 = Node('a', n1)
         self.assertEquals(n1.data, 5)
         self.assertEquals(n2.next_node, n1)
 
     def test_Stack(self):
+        """Class Stack testing"""
         test_assert = Stack()
         self.assertEquals(test_assert.top, None)
         self.assertEquals(test_assert.push("Random Inf"), None)
